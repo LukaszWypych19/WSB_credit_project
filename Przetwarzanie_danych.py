9ecd47057a9843b85c7c59f2e1ed7a4017eb2a08
Luk <lukasz.wypych19@gmail.com>
2023-04-24 10:47:17 +0200

    Script for writing selected data from dataset into the database.Added txt coding, more data writing - only capitalc

--- Przetwarzanie_danych.py
+++ Przetwarzanie_danych.py
@@ -1,19 +1,29 @@
 import csv
 import mysql.connector
+
 # import json
 
 with mysql.connector.connect(user='root', password='admin', host='localhost', database='filip') as connection:
     cursor = connection.cursor()
 
+    # czytamy plik csv za pomoca metody dictreader
     with open('worldcities.csv', encoding='utf-8') as f:
         reading = csv.DictReader(f)
 
+        # w kolumnie city_ascii zamieniamy "'" na " "
         for row in reading:
             city_name = row['city_ascii'].replace("'", " ")
-            sql = f"""INSERT INTO
-            countries_capitals(country, city, capital)
-            VALUES('{row['country']}', '{city_name}', '{row['capital']}');
-            """
-            cursor.execute(sql)
 
-        connection.commit()
+        # wybieramy z tabeli tylko te wiersze w ktorych sa stolice czyli w kolumnie capital wpisane jest primary
+        # tworzymy listy zawierajace panstwo,miasto i czy jest stolica z kazdego wiersza
+            if row['capital'] == 'primary':
+                list_of_c_c = [row['country'], city_name, row['capital']]
+                # print(list_of_c_c)
+
+                sql = f"""INSERT INTO
+                countries_capitals(country, city, capital)
+                VALUES('{list_of_c_c[0]}', '{city_name}', '{list_of_c_c[2]}');
+                """
+                cursor.execute(sql)
+
+            connection.commit()
--- testy.py
+++ testy.py
@@ -1,21 +1,28 @@
 import csv
-# import mysql.connector
+import mysql.connector
 # import json
 
-# with mysql.connector.connect(user='root', password='admin', host='localhost', database='filip') as connection:
-#     cursor = connection.cursor()
+with mysql.connector.connect(user='root', password='admin', host='localhost', database='filip') as connection:
+    cursor = connection.cursor()
 
-with open('worldcities.csv', encoding='utf-8') as f:
-    reading = csv.DictReader(f)
+# czytamy plik csv za pomoca metody dictreader
+    with open('worldcities.csv', encoding='utf-8') as f:
+        reading = csv.DictReader(f)
 
-    for row in reading:
-        city_name = row['city_ascii'].replace("'", " ")
+# w kolumnie city_ascii zamieniamy "'" na " "
+        for row in reading:
+            city_name = row['city_ascii'].replace("'", " ")
 
+# wybieramy z tabeli tylko te wiersze w ktorych sa stolice czyli w kolumnie capital wpisane jest primary
+# tworzymy listy zawierajace panstwo,miasto i czy jest stolica z kazdego wiersza
+            if row['capital'] == 'primary':
+                list_of_c_c = [row['country'], city_name, row['capital']]
+                print(list_of_c_c)
 
-        # sql = f"""INSERT INTO
-        # c_c(capital)
-        # VALUES('{row["city_ascii"]}');
-        # """
-        #     cursor.execute(sql)
-        #
-        # connection.commit()
+                sql = f"""INSERT INTO
+                c_c(country, city, capital)
+                VALUES('{list_of_c_c[0]}', '{city_name}', '{list_of_c_c[2]}');
+                """
+                cursor.execute(sql)
+
+            connection.commit()
