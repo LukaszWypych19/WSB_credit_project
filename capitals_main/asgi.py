8d11029af3df8cd209ed9a5ceb18a0e096902f56
Luk <lukasz.wypych19@gmail.com>
2023-06-17 20:13:22 +0200

    Working app (quiz-game) - add dependencies to pyproject.toml (poetry), move project files to projekt_zaliczeniowy_wsb directory

--- capitals_main/asgi.py
+++ capitals_main/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for capitals project.
+ASGI config for capitals_main project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capitals.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capitals_main.settings')
 
 application = get_asgi_application()
--- game/urls.py
+++ game/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from . import views, controller
-
+from . import views
 
 app_name = 'game'
 urlpatterns = [
